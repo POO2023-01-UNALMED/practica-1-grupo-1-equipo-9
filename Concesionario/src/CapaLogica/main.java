feba05f59b80a4e9995eb39fa63a8a734ef44927
fevelezf <fevelezf@unal.edu.co>
2023-04-03 21:13:53 -0500

    4

--- Concesionario/src/CapaLogica/main.java
+++ Concesionario/src/CapaLogica/main.java
@@ -3,11 +3,8 @@ package CapaLogica;
 public class main {
 
 	public static void main(String[] args) {
-		Auto a1 = new Auto("Chevrolet", 23000000, 2500, "verde azumadrardo", true, true);
-		Auto a2 = new Auto("Chevrolet", 23000000, 2500, "verde azumadrardo", true, false);
-		Auto a3 = new Auto("Chevrolet", 23000000, 2500, "verde azumadrardo", true, false);
-		Auto a4 = new Auto("Chevrolet", 23000000, 2500, "verde azumadrardo", true, false);
-
+		Auto a1= new Auto("Cupra", 20000000, 5400, "verde fofo", true, true);
+		
 		System.out.print(InventarioAuto.autosDisponibles());
 
 	}
