5ceada5d4cabf2db998d649df94180914f1ee1a3
fevelezf <fevelezf@unal.edu.co>
2023-05-23 19:59:45 -0500

    popo

--- Concesionario/src/uiMain/main.java
+++ Concesionario/src/uiMain/main.java
@@ -9,6 +9,7 @@ import java.time.format.TextStyle;
 
 public class main {
 	public static void main(String[] args) {
+
 		Scanner sc = new Scanner(System.in);	
 		boolean volverAlMenuPrincipal = true;
 		/*INTERFAZ*/
