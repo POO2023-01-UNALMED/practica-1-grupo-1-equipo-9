4d89f0152cc472f44deed0f0183dcba98e741e52
sflor3z <sflorezag@unal.edu.co>
2023-06-19 16:31:55 -0500

    puje

--- PYTHON/Concesionario/uigMain/main.py
+++ PYTHON/Concesionario/uigMain/main.py
@@ -29,6 +29,7 @@ from baseDatos.deserializador import Deserializador
 from FieldFrame import FieldFrame
 from gestorAplicacion.Personal.vendedor import Vendedor
 from gestorAplicacion.Activos.transaccionventa import TransaccionVenta
+from gestorAplicacion.Personal.trabajador import Trabajador
 import datetime
 
 if __name__ == "__main__":
@@ -459,19 +460,22 @@ if __name__ == "__main__":
 
                 vendedor_confirmado = vendedores_encontrados[vendedor_elegido]
 
-                carro_confirmado.set_dueno(cliente)
-                carro_confirmado.set_disponible(False)
-                cliente.set_auto(carro_confirmado)
-                vendedor_confirmado.confirmar_venta()
-                deducido = cliente.get_presupuesto()-carro_confirmado.get_precio()
-                cliente.set_presupuesto(deducido)
-                transfer = int(random.random() * 1000)
-
-                info = f"El vendedor es: {vendedor_confirmado.info()} \n"
-                info2 = TransaccionVenta("efectivo", carro_confirmado.get_precio(), cliente, carro_confirmado, vendedor_confirmado, transfer).info()
-                texto = info + info2
-                campo_texto.config(text=texto)
-                boton_aceptar.destroy()
+                if(cliente.get_presupuesto()>=carro_confirmado.get_precio()):
+                    carro_confirmado.set_dueno(cliente)
+                    carro_confirmado.set_disponible(False)
+                    cliente.set_auto(carro_confirmado)
+                    vendedor_confirmado.confirmar_venta()
+                    deducido = cliente.get_presupuesto()-carro_confirmado.get_precio()
+                    cliente.set_presupuesto(deducido)
+                    transfer = int(random.random() * 1000)
+                    info = f"El vendedor es: {vendedor_confirmado.info()} \n"
+                    info2 = TransaccionVenta("efectivo", carro_confirmado.get_precio(), cliente, carro_confirmado, vendedor_confirmado, transfer).info()
+                    Trabajador.pago(vendedor_confirmado, carro_confirmado)
+                    texto = info + info2
+                    campo_texto.config(text=texto)
+                    boton_aceptar.destroy()
+                else:
+                    Exception(messagebox.showwarning("Presupuesto insuficiente", "El cliente no tiene el presupuesto suficiente."))
 
 
 
